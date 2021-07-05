bb287ed4672765faf0dec9d4805bf5191fa28a01
shoeb-howlader <mdshoebhawlader@gmail.com>
2021-07-05 20:34:19 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <h1>Your Upcoming Destinations</h1>
+  <div class="location-contain">
+  <city v-for="city in locations" :key="city.name" :img="city.img" :name="city.name">
+    <p>{{city.desc}}</p>
+  </city>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import City from './components/city.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+    data() {
+    return {
+      locations: [
+        {
+          name: 'moscow',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+         {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`
+        },
+         {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `By the end of the 12th century, Paris had become the political, economic, religious, and cultural capital of France. Maurice de Sully undertook the construction of the Notre Dame Cathedral at its eastern extremity.`
+        },
+      ]
+    }
+  },
+  components:{
+    City
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: 'NTR', sans-serif;
+  background: #eee;
+}
+
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.location-contain {
+  display: flex;
+  justify-content: center;
+}
+
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 360px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+  text-align: justify;
+  h2 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    margin: 10px;
+    align-self: center;
+  }
 }
 </style>
---
+++ src/components/city.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="locations">
+       <div class="place">
+        <img :src="img" width="235" height="300"/>
+        <h2>{{name}}</h2>
+         <slot></slot>
+        
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name:'city',
+       props:['img', 'name'] 
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
